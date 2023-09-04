6260e625da080a5f2bfa2358d43b8dd6bfa16ae9
oh-minyoung90 <ohmy0418@naver.com>
2023-09-04 11:33:15 +0900

    feat: minify 내용 추가, 미디어쿼리 예시 작성

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
-  theme: {}
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+    }
+  }
 };
